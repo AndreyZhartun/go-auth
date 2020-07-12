7325356d4228542eb04694dfd41dcbd13a0f5d7d
Andrey Zhartun <qeetree@gmail.com>
2020-07-12 20:46:38 +0600

    коммит для heroku

---
+++ ignore_keys.go
@@ -0,0 +1,5 @@
+package main
+
+var connString = "mongodb+srv://azadmin:saJgPQmeekDwE5S@az-cluster.9qxft.mongodb.net/goauth?retryWrites=true&w=majority"
+
+var jwtKey = []byte("why-you-have-to-be-mad")
