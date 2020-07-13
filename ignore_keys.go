34ca9228c6c5f2a70a92923f36cda7c0813e8447
Andrey Zhartun <qeetree@gmail.com>
2020-07-13 12:59:13 +0600

    константы в config vars heroku

--- ignore_keys.go
+++ ignore_keys.go
@@ -1,5 +1,9 @@
 package main
 
-var connString = "mongodb+srv://azadmin:saJgPQmeekDwE5S@az-cluster.9qxft.mongodb.net/goauth?retryWrites=true&w=majority"
+import "os"
 
-var jwtKey = []byte("why-you-have-to-be-mad")
+//var connString = "mongodb+srv://azadmin:saJgPQmeekDwE5S@az-cluster.9qxft.mongodb.net/goauth?retryWrites=true&w=majority"
+var connString = string(os.Getenv("CONNSTR"))
+
+//var jwtKey = []byte("why-you-have-to-be-mad")
+var jwtKey = []byte(string(os.Getenv("SECRET")))
